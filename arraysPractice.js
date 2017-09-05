2bb9e4bc1357dfda0c1513a500cefe11fb0753e2
Thomas Lowry <minilao94@yahoo.com>
2017-09-05 17:26:38 -0600

    almost finished

--- arraysPractice.js
+++ arraysPractice.js
@@ -39,7 +39,7 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 //Write a function called reversedLooper that is given letters as it's only argument and loops through the given array backwards alerting every item in the array starting at the end.
 
   //Code Here
-var reversedeLooper = letters => {
+var reversedLooper = letters => {
   for (var i = letters.length-1; i >= 0; i--) {
     alert(letters[i]);
   }
@@ -98,14 +98,13 @@ var divider = numbersArray => {
 var getRandomArbitrary = function() {
   return Math.floor(Math.random() * 30);
 };
-// var numbers = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
+var numbers = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 //Above you're given a function that will return a random number between 0 and 30.  There is also a commented out array full of numbers to help you visualize what your function will be receiving.
 
 // Your job is to write a function named finder that will get a random number (by invoking getRandomArbitrary), then loop through the array (that will be passed in as a parameter) to see if that random number is in the array. If it is, return true, if it's not, return false
 
   //Code Here
-var finder = (num, arr) => arr.indexOf(num) === -1 ? "false" : "true";
-
+var finder = (arr) => arr.indexOf(getRandomArbitrary()) === -1 ? false : true;
 
 
 //Next Problem
@@ -135,6 +134,12 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
     return myGroceryList;
   }
 
+  var addItem = (myGroceryList, item) => {
+    if (myGroceryList.indexOf(item) === -1) {
+      myGroceryList.push(item);
+    }
+    return myGroceryList;
+  }
 
 
 //removeItem(myGroceryList, 'chips') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs'];
@@ -149,7 +154,13 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 //Write a function called maker that creates an array, fills that array with numbers from 1 to 215, then returns the array.
 
   //Code Here
-
+var maker = () => {
+  let arr = [];
+  for (var i = 1; i < 216; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
 
 
 //Next Problem
@@ -160,7 +171,7 @@ var numbers = [5, '9', 16, 19, '25', '34', 48];
 //array after adding ten to each item in numbers. *Verify your answer is correct. --> [15, 19, 26, 29, 35, 44, 58]
 
   //Code Here
-
+var addTen = numbers => numbers.map(a => parseInt(a) + 10);
 
 
 //Next Problem
@@ -181,7 +192,7 @@ for(var i = 0; i < num2; i++){
 //Write a function called 'longer' that is given arr1 and arr2 as it's only arguments. Return the array which is longest.
 
   //Code Here
-
+var longer = (arr1,arr2) => arr1.length > arr2.length ? arr1 : arr2;
 
 /*
 As a continuation of the previous problem, write another function called 'both'.
@@ -195,7 +206,7 @@ Example: var arr1 = [1,2,3,4]; var arr2 = [2,4,5,6]; newArray // [2,4]
 
   //Code Here
 
-var both = (arr1,arr2) => 
+var both = (arr1,arr2) => arr2.filter((n) => arr1.indexOf(n) !== -1);
 
 
 //NEXT PROBLEM
@@ -235,11 +246,14 @@ sure that it's equal to 4. */
 
   //Code Here
 
+  devMountainEmployees.push([tyler, cahlan, ryan, colt]);
+  console.log(devMountainEmployees.length);
+
 /*Now let's say Cahlan has a mental breakdown and has to take a leave of absence to 'find himself'.
 Loop through your devMountainEmployees until you find cahlan, then remove him from the array.*/
 
   //Code Here
-
+devMountainEmployees.filter((n) => n.name !== 'Cahlan');
 
 
 
@@ -252,6 +266,7 @@ of Data is to have an Array full of objects. */
 //Create an empty array called users.
 
   //Code Here
+  var users = [];
 
 /*Now add three user objects to your users array. Each user object should contain the
 following properties. name, email, password, username.*/
@@ -266,6 +281,23 @@ var user1 = {
 
 //Your Code Here
 
+users.push(user1);
+
+users.push({
+  name: 'Thomas',
+  email: 'thomas@yahoo.com',
+  password: '123456',
+  username: 'tommydreamer57'
+});
+
+users.push({
+  name: 'Collin',
+  email: 'collin@yahoo.com',
+  password: '123456',
+  username: 'collin2000'
+});
+
+
 /*Now you have a very common data structure. Twitter is a good use case.
 It's easy to imagine that your followers list on Twitter is an Array full or objects
 and those objects contain properties about the specific person you follow.*/
@@ -275,5 +307,7 @@ objects until you find Tyler's account (use tylermcginnis33@gmail.com to find hi
 Once you find the particular index he's located in, delete him from the array.*/
 
   //Code Here
+    users.filter(a => a.email !== 'tylermcginnis33@gmail.com');
+
 
 //The activity we just did is very much how data works in 'the real world'.
